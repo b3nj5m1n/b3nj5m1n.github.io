392190132961046e39af0f291f4a08989fc642fb
b3nj4m1n <b3nj4m1n@gmx.net>
2022-02-15 03:07:48 +0100

    Update Submodules

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+/bin/bash "./update-modules.sh"
+
+path="$PWD"
+
+modules_path="src/modules"
+
+modules_3style_path="3-style-trainer"
+bash -c "cd $path/$modules_path/$modules_3style_path && yarn && yarn parcel build src/index.html"
+
