de946a580d308f30c9152485a59ee3660809a21a
b3nj4m1n <b3nj4m1n@gmx.net>
2022-02-17 10:37:39 +0100

    Update Submodules

--- update.sh
+++ update.sh
@@ -7,5 +7,5 @@ path="$PWD"
 modules_path="src/modules"
 
 modules_3style_path="3-style-trainer"
-bash -c "cd $path/$modules_path/$modules_3style_path && yarn && yarn parcel build src/index.html --public-url ./ && rm -rf ../../projects/3-style-trainer/ && mkdir -p ../../projects/3-style-trainer/ && cp -r * ../../projects/3-style-trainer/"
+bash -c "cd $path/$modules_path/$modules_3style_path && yarn && yarn parcel build src/index.html --public-url ./ && rm -rf ../../projects/3-style-trainer/ && mkdir -p ../../projects/3-style-trainer/ && cp -r dist/* ../../projects/3-style-trainer/"
 
